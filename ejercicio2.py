ca993eebf1007060e81a8c903704df9562c8ca81
Jhon Jesus <1557983@senati.pe>
2025-02-27 18:41:31 -0500

    actualizado

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,4 @@
-a = [int(input(f"numlist1{i+1}lista1:")for i in range (5))]
-b = [int(input(f"numlist2{i+1}lista2:")for i in range (5))]
-print([a()])
+a=[int(input(f"listanum1{i+1}lista1:")for i in range (5))]
+b=[int(input(f"listanum2{i+1}lista2:") for i in range (5))]
+
+print([a[i] * b[i] for i in range(5)])
--- pipi.py
+++
@@ -1,4 +0,0 @@
-a=[int(input(f"listanum1{i+1}lista1:")for i in range (5))]
-b=[int(input(f"listanum2{i+1}lista2:") for i in range (5))]
-
-print([a[i] * b[i] for i in range(5)])
